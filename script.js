b2aff2f76eb2fcaa01b12c9d83853b11f5547085
SaiSharanya-16 <saisharanya310@gmail.com>
2026-01-28 10:23:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Dark / Light Mode Toggle
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  toggleBtn.textContent =
+    document.body.classList.contains("light") ? "☀️" : "🌙";
+});
